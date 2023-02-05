2a104d5f2647d5a07fdd07c097f9cdbae10e6f72
gulchintory <avgulcin@gmail.com>
2023-02-05 17:08:11 +1100

    deploying

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -10,6 +10,7 @@ function Success() {
   useEffect(() => {
     async function saveBooking() {
       const booked = await idbPromise('booked', 'get');
+      console.log({booked})
       const pets = booked.map((item) => item._id);
 
       if (pets.length) {
